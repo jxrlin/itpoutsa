17e8b28e3203122634770ca0c1a1438161e08b4c
Zaw Linn Htet <zawlinnhtet958@gmail.com>
2025-02-15 02:42:52 +0630

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        base: '/',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/react/main.tsx'],
